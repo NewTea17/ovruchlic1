b328c1d0c1e67922aa53d9d1c5e426f83ac2924a
NewTea17 <118597764+NewTea17@users.noreply.github.com>
2022-12-12 14:40:01 +0200

    Delete swiper.js

--- swiper.js
+++
@@ -1,9 +0,0 @@
-new Swiper('.many-info', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    slidesPerView: 1,
-    spaceBetween: 300,
-    loop: true,
-});
