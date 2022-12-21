d3327b54ba9ce56bf5aee8e21856b255a4ecde82
NewTea17 <118597764+NewTea17@users.noreply.github.com>
2022-12-21 12:03:27 +0200

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,9 @@
+new Swiper('.many-info', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1,
+    spaceBetween: 300,
+    loop: true,
+});
